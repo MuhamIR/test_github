02ed5d1202cef8f554b3440d15287aad7f1748b6
MuhamIR <ilnur.muham@gmail.com>
2016-01-17 11:12:59 +0300

    new feature

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include <cstdlib> // для system
 using namespace std;
 
+void say_hello()
+{
+	cout << "Hello, world!" << endl;
+}
+
 int main() 
 { 
-    cout << "Hello, world!" << endl;
+    say_hello();
     system("pause"); // Только для тех, у кого MS Visual Studio
     return 0; 
 }
